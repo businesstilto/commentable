dcb60e6df8b7afe41a669d8337cd606778791756
matildasmets <101006467+matildasmets@users.noreply.github.com>
2026-01-23 09:32:38 +0000

    Fix styling

--- src/Livewire/Comment.php
+++ src/Livewire/Comment.php
@@ -73,7 +73,7 @@ class Comment extends Component implements HasForms
                         ->toolbarButtons($this->toolbarButtons)
                         ->required()
                         ->maxLength(65535)
-                        ->mentions(fn() => $this->mentions ?? [])
+                        ->mentions(fn () => $this->mentions ?? [])
                         ->fileAttachmentsDisk($this->fileAttachmentsDisk)
                         ->fileAttachmentsDirectory($this->fileAttachmentsDirectory)
                         ->fileAttachmentsAcceptedFileTypes($this->fileAttachmentsAcceptedFileTypes)
@@ -82,7 +82,6 @@ class Comment extends Component implements HasForms
                 ->statePath('data');
         }
     }
-
 
     public function openEdit()
     {
