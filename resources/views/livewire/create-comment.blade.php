0ef663ff1a45d1e3554101ef43a753ac2a3db7ae
matilda <uarpnn@gmail.com>
2026-01-26 09:54:35 +0100

    Use alpine.js form to prevent the submit from closing a modal

--- resources/views/livewire/create-comment.blade.php
+++ resources/views/livewire/create-comment.blade.php
@@ -1,13 +1,18 @@
 <div>
-    <form wire:submit="create">
+    <div x-data="{ submitting: false }">
         {{ $this->form }}
 
         <div @if($buttonPosition === 'right') class="fi-create-comment-actions" @endif>
-            <x-filament::button type="submit" class="fi-create-comment-submit">
+            <x-filament::button
+                wire:click="create"
+                x-on:click="submitting = true"
+                ::disabled="submitting"
+                class="fi-create-comment-submit"
+            >
                 {{ __('commentable::translations.buttons.post') }}
             </x-filament::button>
         </div>
-    </form>
+    </div>
 
     <x-filament-actions::modals />
 </div>
